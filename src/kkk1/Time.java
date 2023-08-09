f284cc81881830d3dc9e79f00233139fe665fe7f
bhavnachoudhari <bhavnachoudhari1234@gmail.com>
2023-08-09 16:52:01 +0530

    my first project

---
+++ src/kkk1/Time.java
@@ -0,0 +1,16 @@
+package kkk1;
+
+import org.testng.annotations.Test;
+
+public class Time {
+	@Test
+	public void m1()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void m2()
+	{
+		System.out.println("bhumi");
+	}
+}
